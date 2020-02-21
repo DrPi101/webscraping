9c7cc41ae0c2a265b4589305722fdaf493d7078b
Dr Pi <59804556+DrPi101@users.noreply.github.com>
2020-02-21 16:17:42 +0000

    Update ebayapi.py

--- ebayapi.py
+++ ebayapi.py
@@ -1,3 +1,4 @@
+import json
 from ebaysdk.finding import Connection as finding # install with pip
 from bs4 import BeautifulSoup # install with pip
 from ebayapi import ebayapi # I have saved into another file to protect my key
@@ -12,36 +13,34 @@ soup = BeautifulSoup(response.content, 'lxml')
 totalentries = int(soup.find('totalentries').text)
 items = soup.find_all('item')
 
-print()
-print()
+def res_print():
+    for item in items:
+        cat = item.categoryname.string.lower()
+        title = item.title.string.lower().strip()
+        price = int(round(float(item.currentprice.string)))
+        url = item.viewitemurl.string.lower()
+        seller = item.sellerusername.text.lower()
+        listingtype = item.listingtype.string.lower()
+        condition = item.condition
+        #displayname = displayname.string.lower()
 
-for item in items:
-    cat = item.categoryname.string.lower()
-    title = item.title.string.lower().strip()
-    price = int(round(float(item.currentprice.string)))
-    url = item.viewitemurl.string.lower()
-    seller = item.sellerusername.text.lower()
-    listingtype = item.listingtype.string.lower()
-    condition = item.condition
-    #displayname = displayname.string.lower()
-
-    print(cat)
-    print("\n")
-    print(title)
-    print()
-    print(price)
-    print()
-    print(url)
-    print()
-    print(seller)
-    print("*" * 20)
+        print(cat)
+        print("\n")
+        print(title)
+        print()
+        print(price)
+        print()
+        print(url)
+        print()
+        print(seller)
+        print("*" * 20)
 
 
-ebay_results = 'ebay_results.txt'
+ebay_results = 'ebay_results.txt' # (Next : Write it to JSON as well as txt)
 
 def res_text():
     with open(ebay_results, "a") as f:
-        for i in items:
+        for item in items:
             cat = item.categoryname.string.lower()
             title = item.title.string.lower().strip()
             price = int(round(float(item.currentprice.string)))
@@ -57,7 +56,21 @@ def res_text():
             f.write("*" * 20)
             f.write("\n")
 
+
+def res_json():
+    pass
+
+res_print()
 res_text()
+            
+
+        
+
+
+    
+    
+
+
             
 
         
