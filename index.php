c2d4668183bd2f06a7095af103c382ed3d962237
Dr Pi <59804556+DrPi101@users.noreply.github.com>
2020-02-21 15:15:59 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
   echo "<br />"; // creating a new line
   echo "<a href=https://www.youtube.com/channel/UCtvGCaEzQZX7GwUJi-E3ALQ>Dr Pi YouTube</a>";
   echo "<br />"; // creating a new line
-
-  echo "<a href="python-test.py">Python App</a>";
-
 ?>
+
+
+
+
+
